80d40aad43bb85e60460c6d91f0926cc42a821b3
bsomnathe <bsomnathe@github.com>
2022-08-28 13:01:08 +0530

    uploaded all

--- hello_world.c
+++ hello_world.c
@@ -2,5 +2,8 @@
 int main()
 {
 	printf("Hello World!!!!");
+	printf("I will going to be good programmer..");
+	printf("I will strugle and looking forward with positive approch");
+	printf("Never give up!!");
 	return 0;
 }
