32d0add93a1cb27ebce8dcbc406eab5e178bc553
bsomnathe <bsomnathe@github.com>
2022-08-28 12:53:00 +0530

    edited in hello world

---
+++ hello_new.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	printf("Hello World!!!!");
+	printf("I will going to be good programmer..");
+	printf("I will strugle and looking forward with positive approch");
+	printf("Never give up!!");
+	return 0;
+}
